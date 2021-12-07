9363955652f6c541336383f409ec1f29eb422783
Phong <92314851+PhonggNguyenn@users.noreply.github.com>
2021-12-07 15:50:35 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -125,7 +125,7 @@ $('#yes').click(function() {
                 text: "Bấm xong thì nhớ bấm inbox nhaaaa :3 iu",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
-                    window.location = 'https://m.me/100055047062562/';
+                    window.location = 'https://m.me/100055225954546/';
                   }
             })
         }
